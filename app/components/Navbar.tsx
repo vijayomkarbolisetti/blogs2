7e8246a66a8ba33d977e37cc24f276815fb5b6f0
vijayomkarbolisetti <163376965+vijayomkarbolisetti@users.noreply.github.com>
2025-03-06 11:25:47 +0530

    update

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
   return (
     <nav className="w-full relative flex items-center justify-between mx-auto px-4 py-5">
       <Link href="/" className="font-bold text-3xl">
-        Marshal<span className="text-primary">Blog</span>
+      
       </Link>
 
 
